1ab4fb70159aad62cd954b9bee5d14075d72dd00
Venkatesh <70619384+venkat-125@users.noreply.github.com>
2025-09-19 13:06:22 +0530

    Add files via upload

---
+++ js/rev-custom.js
@@ -0,0 +1,209 @@
+/*
+Template: Textica - Textile & Fabric Industry HTML Template
+Author: Peacefulqode.com
+Version: 1.0
+Design and Developed by: Peaceful Qode
+*/
+
+// ================Index=========================//
+
+var tpj = jQuery;
+if (window.RS_MODULES === undefined) window.RS_MODULES = {};
+if (RS_MODULES.modules === undefined) RS_MODULES.modules = {};
+RS_MODULES.modules["revslider31"] = {
+    once: RS_MODULES.modules["revslider31"] !== undefined ? RS_MODULES.modules["revslider31"].once : undefined, init: function () {
+        window.revapi3 = window.revapi3 === undefined || window.revapi3 === null || window.revapi3.length === 0 ? document.getElementById("rev_slider_3_1") : window.revapi3;
+        if (window.revapi3 === null || window.revapi3 === undefined || window.revapi3.length == 0) { window.revapi3initTry = window.revapi3initTry === undefined ? 0 : window.revapi3initTry + 1; if (window.revapi3initTry < 20) requestAnimationFrame(function () { RS_MODULES.modules["revslider31"].init() }); return; }
+        window.revapi3 = jQuery(window.revapi3);
+        if (window.revapi3.revolution == undefined) { revslider_showDoubleJqueryError("rev_slider_3_1"); return; }
+        revapi3.revolutionInit({
+            revapi: "revapi3",
+            DPR: "dpr",
+            sliderLayout: "fullwidth",
+            visibilityLevels: "1240,1024,778,480",
+            gridwidth: "1300,1024,778,480",
+            gridheight: "1040,768,650,450",
+            lazyType: "smart",
+            perspective: 600,
+            perspectiveType: "global",
+            editorheight: "1040,768,650,450",
+            responsiveLevels: "1240,1024,778,480",
+            progressBar: { disableProgressBar: true },
+            navigation: {
+                wheelCallDelay: 1000,
+                onHoverStop: false,
+                arrows: {
+                    enable: true,
+                    tmp: "<div class=\"tp-title-wrap\">  	<div class=\"tp-arr-imgholder\"></div> </div>",
+                    style: "zeus",
+                    hide_onmobile: true,
+                    hide_under: "1399px",
+                    hide_onleave: true,
+                    animSpeed: "100ms",
+                    animDelay: "100ms",
+                    left: {
+                        h_offset: 30,
+                        v_offset: 60
+                    },
+                    right: {
+                        h_offset: 30,
+                        v_offset: 60
+                    }
+                },
+                bullets: {
+                    enable: true,
+                    tmp: "",
+                    style: "hesperiden",
+                    hide_over: "1399px",
+                    v_offset: 10
+                }
+            },
+            viewPort: {
+                global: true,
+                globalDist: "-200px",
+                enable: false
+            },
+            fallbacks: {
+                allowHTML5AutoPlayOnAndroid: true
+            },
+        });
+
+    }
+} // End of RevInitScript
+if (window.RS_MODULES.checkMinimal !== undefined) { window.RS_MODULES.checkMinimal(); };
+
+
+// ================Index-2=========================//
+
+var tpj = jQuery;
+if (window.RS_MODULES === undefined) window.RS_MODULES = {};
+if (RS_MODULES.modules === undefined) RS_MODULES.modules = {};
+RS_MODULES.modules["revslider41"] = {
+    once: RS_MODULES.modules["revslider41"] !== undefined ? RS_MODULES.modules["revslider41"].once : undefined, init: function () {
+        window.revapi4 = window.revapi4 === undefined || window.revapi4 === null || window.revapi4.length === 0 ? document.getElementById("rev_slider_4_1") : window.revapi4;
+        if (window.revapi4 === null || window.revapi4 === undefined || window.revapi4.length == 0) { window.revapi4initTry = window.revapi4initTry === undefined ? 0 : window.revapi4initTry + 1; if (window.revapi4initTry < 20) requestAnimationFrame(function () { RS_MODULES.modules["revslider41"].init() }); return; }
+        window.revapi4 = jQuery(window.revapi4);
+        if (window.revapi4.revolution == undefined) { revslider_showDoubleJqueryError("rev_slider_4_1"); return; }
+        revapi4.revolutionInit({
+            revapi: "revapi4",
+            DPR: "dpr",
+            sliderLayout: "fullwidth",
+            visibilityLevels: "1240,1024,778,480",
+            gridwidth: "1300,1024,778,480",
+            gridheight: "1040,768,650,450",
+            lazyType: "smart",
+            perspective: 600,
+            perspectiveType: "global",
+            editorheight: "1040,768,650,450",
+            responsiveLevels: "1240,1024,778,480",
+            progressBar: { disableProgressBar: true },
+            navigation: {
+                wheelCallDelay: 1000,
+                onHoverStop: false,
+                arrows: {
+                    enable: true,
+                    tmp: "<div class=\"tp-title-wrap\">  	<div class=\"tp-arr-imgholder\"></div> </div>",
+                    style: "zeus",
+                    hide_onmobile: true,
+                    hide_under: "1399px",
+                    hide_onleave: true,
+                    animSpeed: "100ms",
+                    animDelay: "100ms",
+                    left: {
+                        h_offset: 30,
+                        v_offset: 60
+                    },
+                    right: {
+                        h_offset: 30,
+                        v_offset: 60
+                    }
+                },
+                bullets: {
+                    enable: true,
+                    tmp: "",
+                    style: "hesperiden",
+                    hide_over: "1399px",
+                    v_offset: 10
+                }
+            },
+            viewPort: {
+                global: true,
+                globalDist: "-200px",
+                enable: false
+            },
+            fallbacks: {
+                allowHTML5AutoPlayOnAndroid: true
+            },
+        });
+
+    }
+} // End of RevInitScript
+if (window.RS_MODULES.checkMinimal !== undefined) { window.RS_MODULES.checkMinimal(); };
+
+
+// ================Index-3=========================//
+
+var tpj = jQuery;
+if (window.RS_MODULES === undefined) window.RS_MODULES = {};
+if (RS_MODULES.modules === undefined) RS_MODULES.modules = {};
+RS_MODULES.modules["revslider51"] = {
+    once: RS_MODULES.modules["revslider51"] !== undefined ? RS_MODULES.modules["revslider51"].once : undefined, init: function () {
+        window.revapi5 = window.revapi5 === undefined || window.revapi5 === null || window.revapi5.length === 0 ? document.getElementById("rev_slider_5_1") : window.revapi5;
+        if (window.revapi5 === null || window.revapi5 === undefined || window.revapi5.length == 0) { window.revapi5initTry = window.revapi5initTry === undefined ? 0 : window.revapi5initTry + 1; if (window.revapi5initTry < 20) requestAnimationFrame(function () { RS_MODULES.modules["revslider51"].init() }); return; }
+        window.revapi5 = jQuery(window.revapi5);
+        if (window.revapi5.revolution == undefined) { revslider_showDoubleJqueryError("rev_slider_5_1"); return; }
+        revapi5.revolutionInit({
+            revapi: "revapi5",
+            DPR: "dpr",
+            sliderLayout: "fullwidth",
+            visibilityLevels: "1240,1024,778,480",
+            gridwidth: "1300,1024,778,480",
+            gridheight: "900,768,550,450",
+            lazyType: "smart",
+            perspective: 600,
+            perspectiveType: "global",
+            editorheight: "900,768,550,450",
+            responsiveLevels: "1240,1024,778,480",
+            progressBar: { disableProgressBar: true },
+            navigation: {
+                wheelCallDelay: 1000,
+                onHoverStop: false,
+                arrows: {
+                    enable: true,
+                    tmp: "<div class=\"tp-title-wrap\">  	<div class=\"tp-arr-imgholder\"></div> </div>",
+                    style: "zeus",
+                    hide_onmobile: true,
+                    hide_under: "1399px",
+                    hide_onleave: true,
+                    animSpeed: "100ms",
+                    animDelay: "100ms",
+                    left: {
+                        h_offset: 30,
+                        v_offset: 60
+                    },
+                    right: {
+                        h_offset: 30,
+                        v_offset: 60
+                    }
+                },
+                bullets: {
+                    enable: true,
+                    tmp: "",
+                    style: "hesperiden",
+                    hide_over: "1399px",
+                    v_offset: 10
+                }
+            },
+            viewPort: {
+                global: true,
+                globalDist: "-200px",
+                enable: false
+            },
+            fallbacks: {
+                allowHTML5AutoPlayOnAndroid: true
+            },
+        });
+
+    }
+} // End of RevInitScript
+if (window.RS_MODULES.checkMinimal !== undefined) { window.RS_MODULES.checkMinimal(); };
